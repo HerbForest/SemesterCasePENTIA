7d1b131dae47ff901fa1b26c4edd02e31f2e4d49
Signeamadsen97 <signe.amalie@live.dk>
2026-03-17 16:13:51 +0100

    opsætning af veiwmed route til Bygherres Homepage

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [],
-});
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/BBHomePageView',
+      name: 'BBHomePage',
+      component: () => import('@/views/BBHomePageView.vue')
+    },
+  ],
+})
 
 export default router;
