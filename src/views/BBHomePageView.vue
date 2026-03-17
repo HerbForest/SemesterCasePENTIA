2314b14743293179cdd76fb996a75dc1f3ffd1ab
Signeamadsen97 <signe.amalie@live.dk>
2026-03-17 18:22:40 +0100

    opsætning af RoundButton component

--- src/views/BBHomePageView.vue
+++ src/views/BBHomePageView.vue
@@ -1,2 +1,14 @@
-<script setup></script>
-<template></template>
+<script setup>
+import RoundButtons from '@/components/buttons/RoundButtons.vue'
+</script>
+<template>
+
+
+		<!-- Blå knap -->
+	<RoundButtons variant="dark" icon="+" />
+	
+		<!-- Hvid knap-->
+	<RoundButtons variant="light" icon="→" />
+
+
+</template>
