6dfa14d7944c8d445d06acf52ce075841c00d8ef
valde <114578370+HerbForest@users.noreply.github.com>
2026-03-19 09:07:24 +0100

    made square buttons

---
+++ src/components/buttons/SqaureButtons.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+</script>
+<template>
+  <button class="primary">primary</button>
+  <button class="secondary">primary</button>
+</template>
+<style>
+.primary {
+  background-color: #2c687d;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+}
+</style>
