cf5f3a05e7607feb71944ea838c7a765ee458766
valde <114578370+HerbForest@users.noreply.github.com>
2026-03-19 09:29:01 +0100

    added routerlink to main

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { RouterView } from "vue-router";
 </script>
 
 <template>
-    <nav>
-        <RouterLink to="/BBHomePageView">BB Home Page</RouterLink>
-    </nav>
-    <RouterView />
+	<nav>
+		<RouterLink to="/BBHomePageView">BB Home Page</RouterLink>
+		<RouterLink to="/Bl">BL Home Page</RouterLink>
+	</nav>
+	<RouterView />
 </template>
 
 <style scoped></style>
--- src/components/buttons/SqaureButtons.vue
+++ src/components/buttons/SqaureButtons.vue
@@ -2,17 +2,20 @@
 import { ref } from "vue";
 </script>
 <template>
-  <button class="primary">primary</button>
-  <button class="secondary">secondary</button>
+	<button class="primary">primary</button>
+	<button class="secondary">secondary</button>
 </template>
 <style>
 .primary {
-  background-color: #2c687d;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-  height: 2.5rem;
-  padding: 0.5rem 1rem;
+	background-color: #2c687d;
+	color: #fff;
+	border: none;
+	border-radius: 5px;
+	height: 2.5rem;
+	padding: 0.5rem 1rem;
+}
+.primary:hover {
+	background-color: rgba(44, 104, 125, 0.9);
 }
 .secondary {
 }
