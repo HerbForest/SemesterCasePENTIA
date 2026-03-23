d4266b62705d86906a946e2476f38cc1b8b03d7f
valde <114578370+HerbForest@users.noreply.github.com>
2026-03-23 14:30:05 +0100

    edits for main scss colors square buttons.vue scss  and added api import link scss on main.js

--- src/components/buttons/SqaureButtons.vue
+++ src/components/buttons/SqaureButtons.vue
@@ -1,19 +1,24 @@
 <script setup>
 import { ref } from "vue";
 </script>
+
 <template>
 	<button class="primary">primary</button>
 	<button class="secondary">secondary</button>
 </template>
-<style scoped>
+
+<style lang="scss" scoped>
+@use "@/assets/scss/variables" as *;
+
 .primary {
-	background-color: v.$color-primary;
-	color: #fff;
+	background-color: $primary-color;
+	color: $primary-foreground-color;
 	border: none;
 	border-radius: 5px;
 	height: 2.5rem;
 	padding: 0.5rem 1rem;
 }
+
 .primary:hover {
 	background-color: rgba(44, 104, 125, 0.9);
 }
