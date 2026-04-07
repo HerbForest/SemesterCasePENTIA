f75310d5d9280509eff63312d485d1f3d209a8b8
valde <fe13valseh@gmail.com>
2026-04-07 13:36:15 +0200

    created BLLayout

--- src/router/index.js
+++ src/router/index.js
@@ -4,15 +4,22 @@ const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
-			path: "/BBHomePageView",
-			name: "BBHomePage",
-			component: () => import("@/views/BBViews/BBHomePageView.vue"),
+			path: '/BBHomePageView',
+			name: 'BBHomePage',
+			component: () => import('@/views/BBViews/BBHomePageView.vue'),
 		},
 		{
-			path: "/BL",
-			name: "BLHomePage",
-			component: () => import("@/views/BLViews/BLHomePageView.vue"),
-		},
+			path: '/BL',
+			component: () => import('@/layout/BLLayout.vue'),
+			children: [
+				{
+					path: 'homepage',
+					name: 'BLHomePage',
+					component: () => import('@/views/BLViews/BLHomePageView.vue'),
+				}
+			]
+		}
+		,
 	],
 });
 
