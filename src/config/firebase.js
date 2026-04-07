d1c486161d3fabf18c62108339629661caa10704
Signeamadsen97 <signe.amalie@live.dk>
2026-04-07 13:31:36 +0200

    initialize firebase and hosting

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKUZLDC9mI-u-fKc-xKNjc4zlCwSrA0RM",
+  authDomain: "pentiasemestercase.firebaseapp.com",
+  projectId: "pentiasemestercase",
+  storageBucket: "pentiasemestercase.firebasestorage.app",
+  messagingSenderId: "825425105975",
+  appId: "1:825425105975:web:0af1979299a91620afdbb2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
