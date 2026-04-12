d460c00def0a966bac8430ee5dbef99301dedab8
valde <114578370+HerbForest@users.noreply.github.com>
2026-04-12 21:56:16 +0200

    navbar missing profilepicture and size changes

--- src/components/BuilderNavbar.vue
+++ src/components/BuilderNavbar.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { computed } from 'vue';
-import { useRoute } from 'vue-router';
-
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+import profilePicture from "@/assets/profile-byggeleder.jpg";
 
 const route = useRoute();
 
@@ -9,14 +9,28 @@ const PageName = computed(() => route.meta.pageName);
 </script>
 
 <template>
-<nav>
-	<div class ="tekst-center">
-	<h1>Milton</h1>
-	<p>House</p>
-	<h1>{{PageName}}</h1>
-	</div>
-	<div>
-
-	</div>
-</nav>
+	<nav class="navbar">
+		<div class="tekst-center">
+			<h1>Milton</h1>
+			<p>House</p>
+			<h1>{{ PageName }}</h1>
+		</div>
+		<div></div>
+		<img :src="profilePicture" alt="profile picture" />
+	</nav>
 </template>
+
+<style lang="scss">
+@use "@/assets/scss/variables" as *;
+* {
+	margin: 0;
+}
+.navbar {
+	background-color: $primary-color;
+}
+.tekst-center {
+	text-align: center;
+	margin: 0;
+	padding: 0;
+}
+</style>
