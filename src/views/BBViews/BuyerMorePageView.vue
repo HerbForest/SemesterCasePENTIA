dad5c805d675506820a06989a02c88a7aac12826
Signeamadsen97 <signe.amalie@live.dk>
2026-04-21 12:31:15 +0200

    add more page to buyer flow

---
+++ src/views/BBViews/BuyerMorePageView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import ButtonCard from '@/components/ButtonCard.vue';
+</script>
+<template>
+	<div class="layout-bb">
+		<h1>Mere</h1>
+	<ButtonCard buttonTitle="Dokumenter" buttonText="Kontrakter, tegninger og mere" :arrow="true">
+
+	<!-- Brug dette når icon bibliotek installeres -->
+	<!-- <template #icon>
+            <CalendarIcon />
+        </template> -->
+</ButtonCard>
+<ButtonCard buttonTitle="Byggeleder" buttonText="Din byggeleder {{Byggeleder}}" :arrow="true">
+</ButtonCard>
+<ButtonCard buttonTitle="Kontrakter" buttonText="Dine oplysninger" :arrow="true">
+</ButtonCard>
+<ButtonCard buttonTitle="indstillinger" buttonText="App-indstillinger" :arrow="true">
+</ButtonCard>
+</div>
+</template>
