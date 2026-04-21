dc67dd872d53cc70b8e8674d71fb833dbfd2df83
valde <114578370+HerbForest@users.noreply.github.com>
2026-04-21 21:38:49 +0200

    added one card and background image to the homepage.

--- src/views/BlViews/BlHomePageView.vue
+++ src/views/BlViews/BlHomePageView.vue
@@ -1,10 +1,27 @@
 <script setup>
-import SqaureButtons from '@/components/buttons/SqaureButtons.vue';
-
+import SqaureButtons from "@/components/buttons/SqaureButtons.vue";
+import ButtonCard from "@/components/ButtonCard.vue";
+import backgroundImg from "@/assets/hero-construction.jpg";
 </script>
 <template>
 	<div>
-		<SqaureButtons />
+		<div class="hero">
+			<img class="hero__image" :src="backgroundImg" alt="hero-construction" />
+			
+		</div>
+		<ButtonCard buttonTitle="Byggeplan" :arrow="true" />
+
 		<div></div>
 	</div>
 </template>
+<style scoped lang="scss">
+.background {
+	width: 100%;
+	height: 200px;
+	overflow: hidden;
+	&__image {
+		width: 100%;
+		object-position: top;
+	}
+}
+</style>
