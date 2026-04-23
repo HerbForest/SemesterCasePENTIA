3772e57ff2a1a6a4f1b1c768d92721704c7ffb3e
Signeamadsen97 <signe.amalie@live.dk>
2026-04-23 12:00:00 +0200

    props and states defined in progressPhaseCard.vue
    props defined and states defined

---
+++ src/components/cards/PhaseProgressCard.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {ref} from 'vue';
+
+const phaseProps = defineProps({
+	number: { type: Number, required: true },
+	title: { type: String, required: true },
+	description: { type: String, default: '' },
+	status: {
+		type: String,
+		default: 'upcoming', // 'completed' | 'active' | 'upcoming'
+	},
+	date: { type: String, default: '' },
+	progress: { type: Number, default: null },
+	images: { type: Array, default: () => [] },
+	timeLeft: { type: Number, default: null },
+})
+
+const isExpanded = ref(phaseProps.status === 'active');
+
+const toggle = () => {
+	if (phaseProps.status === 'completed' || phaseProps === 'active') {
+			isExpanded.value = !isExpanded.value
+	};
+};
+</script>
+
+<template>
+
+</template>
+
+<style>
+</style>
