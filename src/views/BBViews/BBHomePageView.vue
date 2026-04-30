9010b7193c4fe3bfb4bf9226b90e7eac9bb64aa2
Signeamadsen97 <signe.amalie@live.dk>
2026-04-30 10:28:46 +0200

    seeder toogle removed

--- src/views/BBViews/BBHomePageView.vue
+++ src/views/BBViews/BBHomePageView.vue
@@ -1,30 +1,13 @@
 <script setup>
-import { ref, onMounted  } from 'vue';
+import { onMounted  } from 'vue';
 
-import RoundButtons from '@/components/buttons/RoundButtons.vue';
-import SlideButtons from '@/components/buttons/SlideButtons.vue';
-import Card from '@/components/TextCard.vue';
 import ButtonCard from '@/components/ButtonCard.vue';
 import Header from '@/components/BuilderHeader.vue';
 
-const isToggled = ref(false);
 
 
 import { db } from '@/config/firebase';
 import { doc, getDoc } from 'firebase/firestore';
-
-const message = ref('');
-
-onMounted(async () => {
-	const docRef = doc(db, 'test', 'test1');
-	const docSnap = await getDoc(docRef);
-    
-	if (docSnap.exists()) {
-		message.value = docSnap.data().message;
-	} else {
-		message.value = 'Ingen data fundet';
-	}
-});
 
 // skal rettes til data fra database!!
 const bbTitle = 'Jonas';
@@ -57,20 +40,6 @@ const bbTitle = 'Jonas';
 </ButtonCard>
 <ButtonCard buttonTitle="Kontakt din byggeleder" :arrow="true">
 </ButtonCard>
-
-
-
-		<!-- Blå knap -->
-	<RoundButtons variant="dark" icon="+" />
-	
-		<!-- Hvid knap-->
-	<RoundButtons variant="light" icon="→" />
-
-<SlideButtons v-model="isToggled" />	
-
-	<Card title="Om Thomas">
-		<p>Thomas har over 15 års erfaring...</p>
-	</Card>
 </div>
 </template>
 
