2aef931ebfd777a41de84b75d43f8ed900030731
Signeamadsen97 <signe.amalie@live.dk>
2026-05-01 11:22:41 +0200

    return button component set up

---
+++ src/components/buttons/ReturnButton.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
+
+<template>
+    <button class="back-button" @click="router.back()">
+        ← Tilbage
+    </button>
+</template>
+
+<style scoped lang="scss">
+.back-button {
+    background: none;
+    border: none;
+    color: $foreground-color;
+    font-size: $font-size-lg;
+    font-weight: $font-weight-semibold;
+    cursor: pointer;
+    padding: 0;
+    margin-bottom: 16px;
+}
+</style>
+
