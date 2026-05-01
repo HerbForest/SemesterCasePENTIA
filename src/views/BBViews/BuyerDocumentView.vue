9c7926612794e8d89d6162a23fbe9882184914e6
Signeamadsen97 <signe.amalie@live.dk>
2026-05-01 10:45:53 +0200

    adding store and documents to view

--- src/views/BBViews/BuyerDocumentView.vue
+++ src/views/BBViews/BuyerDocumentView.vue
@@ -1,8 +1,27 @@
 <script setup>
-import DocumentCard from '@/components/cards/DocumentCard.vue';
+import { onMounted } from 'vue'
+import DocumentCard from '@/components/cards/DocumentCard.vue'
+import { useDocumentStore } from '@/stores/documentStore'
+import { useProjectStore } from '@/stores/projectStore'
 
+const documentStore = useDocumentStore()
+const projectStore = useProjectStore()
+
+onMounted(async () => {
+    await documentStore.fetchDocuments(projectStore.project.id)
+})
 </script>
 <template>
-	<DocumentCard />
+	   <div class="layout-bb">
+        <DocumentCard
+            v-for="doc in documentStore.documents"
+            :key="doc.id"
+            :name="doc.name"
+            :size="doc.size"
+            :date="doc.date"
+            :uploadedBy="doc.uploadedBy"
+            :downloadUrl="doc.downloadUrl"
+        />
+    </div>
 
 </template>
