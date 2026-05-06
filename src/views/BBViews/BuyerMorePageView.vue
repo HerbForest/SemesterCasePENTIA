3d053a3eceb1082ba8ba14f7de9f97102e0396c5
Signeamadsen97 <signe.amalie@live.dk>
2026-05-06 12:49:09 +0200

    adding route to profile view

--- src/views/BBViews/BuyerMorePageView.vue
+++ src/views/BBViews/BuyerMorePageView.vue
@@ -9,7 +9,7 @@ import {  FileText, HardHat, UserRound, Settings } from '@lucide/vue';
 	</ButtonCard>
 	<ButtonCard buttonTitle="Byggeleder" buttonText="Din byggeleder" to="/buyer/kontakt" :icon="HardHat" :arrow="true">
 	</ButtonCard>
-	<ButtonCard buttonTitle="Profil" buttonText="Dine oplysninger" to="/buyer/dokumenter" :icon="UserRound" :arrow="true">
+	<ButtonCard buttonTitle="Profil" buttonText="Dine oplysninger" to="/buyer/profil" :icon="UserRound" :arrow="true">
 	</ButtonCard>
 	<ButtonCard buttonTitle="indstillinger" buttonText="App-indstillinger" to="/buyer/dokumenter" :icon="Settings":arrow="true">
 	</ButtonCard>
