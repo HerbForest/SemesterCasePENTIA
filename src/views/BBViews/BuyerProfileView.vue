34d4b2079d0638ed4a07be99d4ca1d1aa02d7f69
Signeamadsen97 <signe.amalie@live.dk>
2026-05-06 12:49:31 +0200

    adding profile card to profile view

--- src/views/BBViews/BuyerProfileView.vue
+++ src/views/BBViews/BuyerProfileView.vue
@@ -1,4 +1,19 @@
-<script></script>
+<script setup>
+import { useBuyerStore } from '@/stores/buyerStore'
+import { useProjectStore } from '@/stores/projectStore'
+import ProfileCard from '@/components/cards/ProfileCard.vue'
+
+const buyerStore = useBuyerStore()
+const projectStore = useProjectStore()
+</script>
 <template>
-<div>hej</div>
+<div class="layout-bb">
+	 <ProfileCard
+        :firstName="buyerStore.buyer?.firstName"
+        :lastName="buyerStore.buyer?.lastName"
+        :email="buyerStore.buyer?.email"
+        :phone="buyerStore.buyer?.phone"
+        :address="projectStore.project?.address"
+    />
+</div>
 </template>
