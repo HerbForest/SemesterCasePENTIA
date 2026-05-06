558589f04d8c078f53d770ed0146662bac395ec6
Signeamadsen97 <signe.amalie@live.dk>
2026-05-06 11:59:18 +0200

    imports and necessary props set up

---
+++ src/components/cards/ProfileCard.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {ref} from 'vue';
+import { UserRound, Mail, Phone, MapPin, Pencil, Check } from '@lucide/vue'
+
+const props = defineProps({
+	firstName: { type: string, default: ''},
+	lastName: { type: string, default: ''},
+	email: { type: string, default: ''},
+	Phone: { type: string, default: ''},
+	address: { type: string, default: ''},
+})
+</script>
+<template></template>
+<style scoped lang="scss"></style>
