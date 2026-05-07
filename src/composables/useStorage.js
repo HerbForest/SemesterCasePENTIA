6891af1644acd5c50929bad6ffc6632de4601364
Signeamadsen97 <signe.amalie@live.dk>
2026-05-07 13:37:50 +0200

    uplad funktion added

--- src/composables/useStorage.js
+++ src/composables/useStorage.js
@@ -8,5 +8,43 @@ export const useStorage = () => {
     const uploadProgress = ref(0)
     const error = ref(null)
 
-    return { uploading, uploadProgress, error }
+const uploadDocument = async (file, projectId, category, uplaoded) => {
+	uploading.value = true;
+	uploadProgress.value = 0;
+	error.value = null
+
+	try{
+		const fileRef = storageRef(storage, `projects/${projectId}/documents/${file.name}`)
+
+		const uploadTask = uploadBytesResumable(fileRef, file)
+
+		 await new Promise((resolve, reject) => {
+        uploadTask.on('state_changed',
+          (snapshot) => {
+              uploadProgress.value = Math.round(
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+          },
+            reject,
+            resolve
+          )
+          })
+					const downloadUrl = await getDownloadURL(fileRef)
+
+          await addDoc(collection(db, 'documents'), {
+            	name: file.name,
+              size: `${Math.round(file.size / 1024)} KB`,
+              date: new Date().toLocaleDateString('da-DK', { day: 'numeric', month: 'long', year: 'numeric' }),
+              category: category,
+              uploadedBy: uploadedBy,
+              projectId: projectId,
+              downloadUrl: downloadUrl
+          })
+					} catch (err) {
+						error.value = err.message
+						console.error('Fejl ved upload:', err)
+						} finally {
+							uploading.value = false
+						}
+			}
+	return { uploading, uploadProgress, error, uploadDocument }
 }
