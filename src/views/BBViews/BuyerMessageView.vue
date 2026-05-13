09bc019b0d97121f589f520709b1b14f24a133bb
Signeamadsen97 <signe.amalie@live.dk>
2026-05-13 10:44:54 +0200

    add unmounted to message view

--- src/views/BBViews/BuyerMessageView.vue
+++ src/views/BBViews/BuyerMessageView.vue
@@ -9,6 +9,20 @@ const buyerStore = useBuyerStore()
 const builderStore = useBuilderStore()
 const projectStore = useProjectStore()
 
+onMounted(async () => {
+    if (!projectStore.project) {
+        await buyerStore.fetchBuyer('dit-bruger-id')
+        await projectStore.fetchProject(buyerStore.buyer.projectId)
+    }
+    if (!builderStore.builder) {
+        await builderStore.fetchBuilder(projectStore.project.builderId)
+    }
+})
+
+const buyerName = computed(() => buyerStore.buyer?.firstName)
+const builderName = computed(() => `${builderStore.builder?.firstName} ${builderStore.builder?.lastName}`)
+const builderImage = computed(() => builderStore.builder?.profileImage)
+
 </script>
 <template>
 	<div>
