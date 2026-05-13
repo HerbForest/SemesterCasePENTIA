0c1ba669e5d1b402a9e965cc926b94c41bd02031
Signeamadsen97 <signe.amalie@live.dk>
2026-05-13 10:07:14 +0200

    add types to script section of message component

---
+++ src/components/cards/MessageCard.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+    type: {
+        type: String,
+        default: 'builder' // 'builder' eller 'buyer'
+    },
+    name: {
+        type: String,
+        default: ''
+    },
+    time: {
+        type: String,
+        default: ''
+    },
+     profileImage: {
+        type: String,
+        default: null  // null = vis initialer i stedet
+    }
+})
+</script>
+<template>
+
+</template>
