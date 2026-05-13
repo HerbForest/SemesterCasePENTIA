41929231eddc33e0e34b1cab0aeb436ae599ad05
Signeamadsen97 <signe.amalie@live.dk>
2026-05-13 10:11:07 +0200

    add left aligned message to tempalte section of message komponent

--- src/components/cards/MessageCard.vue
+++ src/components/cards/MessageCard.vue
@@ -19,5 +19,25 @@ defineProps({
 })
 </script>
 <template>
-
+<div :class="['message-card', `message-card--${type}`]">
+        
+        <!-- Builder besked - venstrestillet med avatar og navn -->
+        <div v-if="type === 'builder'" class="message-card__builder">
+            <img 
+                v-if="profileImage" 
+                :src="profileImage" 
+                class="message-card__avatar"
+                alt="Byggeleder"
+            />
+            <div class="message-card__body">
+                <div class="message-card__meta">
+                    <span class="message-card__name">{{ name }}</span>
+                    <span class="message-card__time">{{ time }}</span>
+                </div>
+                <div class="message-card__bubble">
+                    <slot />
+                </div>
+            </div>
+        </div>
+</div>
 </template>
