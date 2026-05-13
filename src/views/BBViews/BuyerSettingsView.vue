079b2a1aa87a0abaef8c6fc5af2ff56ebc2f93f0
Signeamadsen97 <signe.amalie@live.dk>
2026-05-13 17:44:38 +0200

    add imports to script section of settings view

--- src/views/BBViews/BuyerSettingsView.vue
+++ src/views/BBViews/BuyerSettingsView.vue
@@ -1,2 +1,12 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { Bell, Moon, Globe, Shield, LogOut } from '@lucide/vue'
+import SlideButtons from '@/components/buttons/SlideButtons.vue'
+import Card from '@/components/TextCard.vue'
+import ButtonCard from '@/components/ButtonCard.vue'
+
+const pushNotifications = ref(true)
+const emailUpdates = ref(true)
+const darkMode = ref(false)
+const language = ref('dansk')</script>
 <template></template>
