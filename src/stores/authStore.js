640022e061f576668cab30841b5ba5f6bd6fa354
Signeamadsen97 <signe.amalie@live.dk>
2026-05-14 11:34:50 +0200

    add export to auth store

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -2,3 +2,7 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import { auth } from '@/config/firebase'
 import { signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth'
+
+export const useAuthStore = defineStore('auth', () => {
+    const user = ref(null)
+    const loading = ref(true)
