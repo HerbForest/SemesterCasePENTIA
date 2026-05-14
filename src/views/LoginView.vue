d8b57c6876f3f32af03cc6b23af97375718fbbdf
Signeamadsen97 <signe.amalie@live.dk>
2026-05-14 12:24:32 +0200

    add handler for login on loginView

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -10,4 +10,17 @@ const email = ref('');
 const password = ref('');
 const error = ref();
 const loading = ref(false);
+
+const handleLogin = async () => {
+    error.value = ''
+    loading.value = true
+    try {
+        await authStore.login(email.value, password.value)
+        router.push('/buyer/home') // midlertidigt - skal senere routes baseret på brugertype
+    } catch (err) {
+        error.value = 'Forkert email eller adgangskode'
+    } finally {
+        loading.value = false
+    }
+}
 </script>
