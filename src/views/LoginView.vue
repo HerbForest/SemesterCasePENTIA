7f27fb876bdfeb52aefe3a6e258cc4859fd1e18a
Signeamadsen97 <signe.amalie@live.dk>
2026-05-14 12:21:29 +0200

    add imports and consts to script section of loginView

---
+++ src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useAuthStore } from '@/stores/authStore';
+
+const router = useRouter();
+const authStore = useAuthStore();
+
+const email = ref('');
+const password = ref('');
+const error = ref();
+const loading = ref(false);
+</script>
