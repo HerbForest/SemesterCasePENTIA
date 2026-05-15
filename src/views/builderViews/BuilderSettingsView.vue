1b53a556580ae0103cd811c0abaa145b51be5f0e
Signeamadsen97 <signe.amalie@live.dk>
2026-05-15 12:00:05 +0200

    add imports and data functions to builder settings view

--- src/views/builderViews/BuilderSettingsView.vue
+++ src/views/builderViews/BuilderSettingsView.vue
@@ -1,5 +1,27 @@
-<script setup></script>
+<script setup>
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+import { useAuthStore } from '@/stores/authStore'
+import { useBuilderStore } from '@/stores/builderStore'
+import { Mail, CircleHelp, Globe, Moon, Bell, Shield, LogOut } from '@lucide/vue'
+import ButtonCard from '@/components/cards/ButtonCard.vue'
+import Card from '@/components/cards/TextCard.vue'
+
+const router = useRouter()
+const authStore = useAuthStore()
+const builderStore = useBuilderStore()
+
+const initials = computed(() => {
+    if (!builderStore.builder) return ''
+    return `${builderStore.builder.firstName[0]}${builderStore.builder.lastName[0]}`
+})
+
+const handleLogout = async () => {
+    await authStore.logout()
+    router.push('/login')
+}
+</script>
 
 <template>
-	<div></div>
+	
 </template>
