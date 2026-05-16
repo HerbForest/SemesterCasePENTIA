9741766586befeb229b3353eb01a1dc5ee260345
Signeamadsen97 <signe.amalie@live.dk>
2026-05-16 15:26:21 +0200

    add html and routerLink import

--- src/components/cards/ConversationCard.vue
+++ src/components/cards/ConversationCard.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { RouterLink } from 'vue-router'
+
 defineProps({
     initials: { type: String, default: '' },
     name: { type: String, default: '' },
@@ -9,3 +11,17 @@ defineProps({
     to: { type: [String, Object], default: null }
 })
 </script>
+<template>
+    <component :is="to ? RouterLink : 'div'" :to="to || undefined" class="conversation-card">
+        <div class="conversation-card__avatar">{{ initials }}</div>
+        <div class="conversation-card__content">
+            <p class="conversation-card__name">{{ name }}</p>
+            <p class="conversation-card__address">{{ address }}</p>
+            <p class="conversation-card__message">{{ lastMessage }}</p>
+        </div>
+        <div class="conversation-card__right">
+            <span class="conversation-card__time">{{ time }}</span>
+            <span v-if="unread" class="conversation-card__badge">{{ unread }}</span>
+        </div>
+    </component>
+</template>
