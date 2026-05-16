65478948bb0549cb604fcd9290eb92b415c9d6c5
Signeamadsen97 <signe.amalie@live.dk>
2026-05-16 15:16:23 +0200

    adding props to conversation card

---
+++ src/components/cards/ConversationCard.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({
+    initials: { type: String, default: '' },
+    name: { type: String, default: '' },
+    address: { type: String, default: '' },
+    lastMessage: { type: String, default: '' },
+    time: { type: String, default: '' },
+    unread: { type: Number, default: 0 },
+    to: { type: [String, Object], default: null }
+})
+</script>
