7454651c0b51cfb82d143acc92381683c785d3f3
Signeamadsen97 <signe.amalie@live.dk>
2026-05-17 16:14:40 +0200

    add onmounted data from project store

--- src/views/builderViews/BuilderMessengerView.vue
+++ src/views/builderViews/BuilderMessengerView.vue
@@ -9,6 +9,25 @@ import ConversationCard from '@/components/cards/ConversationCard.vue'
 const authStore = useAuthStore()
 const projectStore = useProjectStore()
 const buyerStore = useBuyerStore()
+
+onMounted(async () => {
+    await projectStore.fetchProjectsByBuilder(authStore.user.uid)
+
+    for (const project of projectStore.builderProjects) {
+        const buyer = await buyerStore.fetchBuyerByProjectId(project.id)
+        if (buyer) {
+            conversations.value.push({
+                initials: `${buyer.firstName[0]}${buyer.lastName[0]}`,
+                name: `${buyer.firstName} ${buyer.lastName}`,
+                address: project.address,
+                lastMessage: 'Tryk for at se samtalen',
+                time: '',
+                unread: 0,
+                to: { name: 'builderChat', params: { id: project.id } }
+            })
+        }
+    }
+})
 </script>
 
 <template>
