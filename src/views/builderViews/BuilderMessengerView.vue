acb7ffbf2bcc58ebc5e403fb061ce32bcb3bdb19
Signeamadsen97 <signe.amalie@live.dk>
2026-05-17 15:54:29 +0200

    add import s to builder messenger view

--- src/views/builderViews/BuilderMessengerView.vue
+++ src/views/builderViews/BuilderMessengerView.vue
@@ -1,5 +1,10 @@
 script setup>
 <script>
+import { onMounted, ref } from 'vue'
+import { useAuthStore } from '@/stores/authStore'
+import { useProjectStore } from '@/stores/projectStore'
+import { useBuyerStore } from '@/stores/buyerStore'
+import ConversationCard from '@/components/cards/ConversationCard.vue'
 </script>
 
 <template>
