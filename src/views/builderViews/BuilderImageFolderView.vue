5cdc0d3fb881ca69734a3226d4d8698d6eb8edf5
Signeamadsen97 <signe.amalie@live.dk>
2026-05-18 15:15:16 +0200

    add imports to BuilderImageFolderView.vue

---
+++ src/views/builderViews/BuilderImageFolderView.vue
@@ -0,0 +1,6 @@
+<script setup>
+import { ref, computed, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { useImageStore } from '@/stores/imageStore'
+import { useTaskStore } from '@/stores/taskStore'
+</script>
