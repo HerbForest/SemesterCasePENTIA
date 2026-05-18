560b2b14a07e19783013d98065bc1b97bfe79cb1
Signeamadsen97 <signe.amalie@live.dk>
2026-05-18 09:26:46 +0200

    add imports to builder chat view

---
+++ src/views/builderViews/BuilderChatView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { useBuilderStore } from '@/stores/builderStore'
+import { useBuyerStore } from '@/stores/buyerStore'
+import { useProjectStore } from '@/stores/projectStore'
+import MessageCard from '@/components/cards/MessageCard.vue'
+import MessageInput from '@/components/MessageInput.vue'
+</script>
+<template>
+	<div></div>
+</template>
