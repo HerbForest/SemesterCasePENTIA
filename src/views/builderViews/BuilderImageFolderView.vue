8f3587d7ec92fc29799385c49c859ce146e2ffea
Signeamadsen97 <signe.amalie@live.dk>
2026-05-18 15:18:34 +0200

    use fetch images by project function to BuilderImageFolderView.vue

--- src/views/builderViews/BuilderImageFolderView.vue
+++ src/views/builderViews/BuilderImageFolderView.vue
@@ -3,4 +3,16 @@ import { ref, computed, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { useImageStore } from '@/stores/imageStore'
 import { useTaskStore } from '@/stores/taskStore'
+
+const route = useRoute()
+const imageStore = useImageStore()
+const taskStore = useTaskStore()
+
+const selectedPhase = ref('alle') // 'alle' eller et fase ID
+
+onMounted(async () => {
+    await imageStore.fetchImagesByProject(route.params.projectId)
+    await taskStore.fetchTasks(route.params.projectId)
+})
+
 </script>
