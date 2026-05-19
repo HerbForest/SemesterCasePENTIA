bc8295b2a875c861eff4d16b8c38c24c70e716ea
Signeamadsen97 <signe.amalie@live.dk>
2026-05-19 20:02:34 +0200

    add documentation to useDocumentStore function in documentStore.js

--- src/stores/documentStore.js
+++ src/stores/documentStore.js
@@ -3,6 +3,10 @@ import { ref } from 'vue';
 import { db } from '@/config/firebase';
 import { collection, query, where, getDocs } from 'firebase/firestore';
 
+/**
+ * Document store til håndtering af dokument data fra Firestore.
+ * Henter og gemmer dokumenter tilknyttet et specifikt projekt.
+ */
 export const useDocumentStore = defineStore('documents', () => {
 	const documents = ref([]);
 	const loading = ref(false);
