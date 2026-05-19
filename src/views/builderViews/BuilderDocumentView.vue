47cfe33dd8ab48eed1f3087c74ff81b863b4705d
Signeamadsen97 <signe.amalie@live.dk>
2026-05-19 11:33:48 +0200

    add imports and document card in html section of  BuilderDocumentView.vue

---
+++ src/views/builderViews/BuilderDocumentView.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { computed, watch } from 'vue'
+import { useRoute } from 'vue-router'
+import { useDocumentStore } from '@/stores/documentStore'
+import { useStorage } from '@/composables/useStorage'
+import ReturnButton from '@/components/buttons/ReturnButton.vue'
+import DocumentCard from '@/components/cards/DocumentCard.vue'
+</script>
+
+<template>
+	<div class="builder-docs">
+    <ReturnButton />
+    <header class="builder-docs__header">
+        <h1 class="builder-docs__title">Dokumenter</h1>
+    </header>
+	   <p class="builder-docs__section-title">KONTRAKTER</p>
+    <DocumentCard
+        v-for="doc in kontrakter"
+        :key="doc.id"
+        :name="doc.name"
+        :size="doc.size"
+        :date="doc.date"
+        :uploadedBy="doc.uploadedBy"
+        :downloadUrl="doc.downloadUrl"
+        :deletable="true"
+        @delete="handleDelete(doc)"
+    />
+    <p v-if="!kontrakter.length" class="builder-docs__empty">Ingen kontrakter uploadet endnu</p>
+	</div>
+</template>
