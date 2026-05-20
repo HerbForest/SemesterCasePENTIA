1ae22a844aa90749c7cb858878e76a7e0b82b5c6
Signeamadsen97 <signe.amalie@live.dk>
2026-05-20 10:13:15 +0200

    add logout component and remove hardcoded button and styling in BuyerMorePageView.vue

--- src/views/buyerViews/BuyerMorePageView.vue
+++ src/views/buyerViews/BuyerMorePageView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import ButtonCard from '@/components/cards/ButtonCard.vue';
+import LogoutButton from '@/components/buttons/LogoutButton.vue';
 import { FileText, HardHat, UserRound, Settings, LogOut } from '@lucide/vue';
 </script>
 <template>
@@ -16,30 +17,7 @@ import { FileText, HardHat, UserRound, Settings, LogOut } from '@lucide/vue';
 			:arrow="true">
 		</ButtonCard>
 
-	
-		<button class="logout">
-			<LogOut :size="18" />
-			Log ud
-		</button>
+		<LogoutButton variant="outline" />
 	</div>
 </template>
-<style scoped lang="scss">
-.logout {
-	width: 100%;
-	padding: 16px;
-	background: $primary-foreground-color;
-	color: $destructive-color;
-	border: 1px;
-	border-style: solid;
-	border-color: $border-color;
-	border-radius: 16px;
-	font-size: $font-size-lg;
-	font-weight: $font-weight-semibold;
-	cursor: pointer;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	gap: 8px;
-	margin-bottom: 12px;
-}
-</style>
+
