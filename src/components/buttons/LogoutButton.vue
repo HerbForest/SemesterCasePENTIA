d8a2f58fbbf00d8f4832c6a91b4f4b557d6687bf
Signeamadsen97 <signe.amalie@live.dk>
2026-05-20 09:31:51 +0200

    add prop as variant for different colored buttons

--- src/components/buttons/LogoutButton.vue
+++ src/components/buttons/LogoutButton.vue
@@ -2,4 +2,11 @@
 import { useRouter } from 'vue-router'
 import { useAuthStore } from '@/stores/authStore'
 import { LogOut } from '@lucide/vue'
+
+defineProps({
+    variant: {
+        type: String,
+        default: 'outline' // 'outline' eller 'filled'
+    }
+})
 </script>
